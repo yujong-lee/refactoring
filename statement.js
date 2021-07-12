f97da50e71b27963a5fcc3c3a91fd5eb6b781f2a
hannut91 <hannut91@gmail.com>
2021-07-13 01:06:37 +0900

    Replace conditional with Polymorphism
    조건문 대신 정의한 Play모델을 이용해 다형성으로 구현했습니다.

--- statement.js
+++ statement.js
@@ -1,3 +1,5 @@
+const { createPlay } = require('./factories/create-play');
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -12,29 +14,8 @@ function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
 
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case 'tragedy': // 비극
-        thisAmount = 40000;
-
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy': // 희극
-        thisAmount = 30000;
-
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르 : ${play.type}`);
-    }
+    const p = createPlay(play.type, perf.audience);
+    const thisAmount = p.calculatePrice();
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
